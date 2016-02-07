3649e165ba8d4551aba4248b7e36f02812b6e220
Zuil <zuilnorris@gmail.com>
2016-02-07 18:37:27 -0430

    Archer ahora si hace critico

--- Main.c
+++ Main.c
@@ -11,24 +11,21 @@ int main()
 {
 	srand(time(NULL));
 	Player Daralion, Mabel, Xetfrick;
-	/*
+	
 	Load_Player("Daralion", &Daralion);
 	Load_Player("Mabel", &Mabel);
 	Load_Player("Xetfrick", &Xetfrick);
-	Load_Player("Hermenegildo",&Herme);
-	
-	Print_Player(Herme);
+	/*
 	Print_Player(Daralion);
 	Print_Player(Mabel);
 	Print_Player(Xetfrick);
-	*/
-	
+	*/	
 	printf("\n%.2f\n",Damage_d(&Daralion.stats,&Mabel.stats));
 	printf("\nHP = %.2f\n", Mabel.stats.HP);
-	/*
+	
 	Save_Player("Daralion", &Daralion);
 	Save_Player("Mabel", &Mabel);
 	Save_Player("Xetfrick", &Xetfrick);
-	*/
+	
 	system("PAUSE");
 }
